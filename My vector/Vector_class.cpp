cd9e447e1c9f79b3c151f921c0c6a8b66b35a900
Doholod <egorcherd16@gmail.com>
2024-11-04 15:03:56 +1000

    ADD: constructor copy, operator=

--- My vector/Vector_class.cpp
+++ My vector/Vector_class.cpp
@@ -17,10 +17,16 @@ template<typename T>vector_yourself<T>::~vector_yourself() {
 //Copy constructor
 template<typename T>vector_yourself<T>::vector_yourself(const vector_yourself& other) {
 	this->size = other.size;
-	delete data;
+	delete this->data;
 	data = new T[this->size];
 	for (size_t i = 0; i < this->size; ++i) {
 		this->data[i] = other.data[i];
 	}
+}
+// Copy assignment operator
+
+template<typename T>
+vector_yourself<T>& operator= (const vector_yourself<T>& other1, const vector_yourself<T>& other2) {
+	if (this == &other2) return *this;
 }
 
--- My vector/vector.h
+++ My vector/vector.h
@@ -11,7 +11,12 @@ namespace my_lib {
 		//base constructors
 		vector_yourself();
 		vector_yourself(size_t f);
+		//destructor
 		~vector_yourself();
+		//constructor copy
 		vector_yourself(const vector_yourself& other);
+		// Copy assignment operator
+		template <typename T>
+		vector_yourself& operator= (const vector_yourself<T>& other1);
 	};
 }
