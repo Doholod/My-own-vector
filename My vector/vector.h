73df855c9849021a6839129d74b049b0274bf886
Doholod <egorcherd16@gmail.com>
2024-11-08 00:18:55 +1000

    ADD: Я изменил план, для программы. В заголовке объявил методы и конструкторы, которые нужно выполнить

--- My vector/vector.h
+++ My vector/vector.h
@@ -4,20 +4,32 @@ namespace my_lib {
 	template <typename Type>
 	class vector_yourself
 	{
-	private:
-		Type* data;
-		size_t size;
 	public:
 		//base constructors
 		vector_yourself();
 		vector_yourself(size_t f);
+
 		//destructor
 		~vector_yourself();
+
 		//constructor copy
 		vector_yourself(const vector_yourself& other);
+
+		// Move constructor
+		vector_yourself(vector_yourself&& other);
+		
+		//Move assignment operator
+		vector_yourself& operator=(vector_yourself&& other)noexcept;
+
 		// Copy assignment operator
 		void operator=(const vector_yourself<Type>& other1);
+		bool operator==(const vector_yourself<Type>& other1);
 		bool operator!=(const vector_yourself<Type>& other1);
+		bool operator*(const vector_yourself<Type>& other1);
+		void operator+ (const vector_yourself<Type>& other1);
+	private:
+		Type* data;
+		size_t size;
 	};
 	 
 }
